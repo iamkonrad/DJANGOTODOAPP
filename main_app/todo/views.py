9d61aacd82a2738f9540ec8f1dbde496f833f39c
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-11 19:02:46 +0200

    Mark as undone button (uncompleted tasks)
    -creating a mark_as_undone function in todo app's views (taking 2 parameters), first fetching the task, then assigning False to it (making it undone) and then saving
    it and returning it into home.html template
    -creating a path in todoapp's to the mark_as_undone function
    -passing the url with a reference to the tasks and dynamic pk into the home.html template

--- main_app/todo/views.py
+++ main_app/todo/views.py
@@ -5,11 +5,18 @@ from .models import Task
 
 def addTask(request):
     task = request.POST['task']                                                                                         #from the button
-    Task.objects.create(task=task)                                                                                               #based on the models
+    Task.objects.create(task=task)                                                                                      #based on the models
     return redirect ('home')
 
 def mark_as_done(request, pk):
     task = get_object_or_404(Task,pk=pk)                                                                                #fetching the object from database if it exists
     task.is_completed=True
+    task.save()
+    return redirect ('home')
+
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task,pk=pk)                                                                                #fetching the object from database if it exists
+    task.is_completed=False
     task.save()
     return redirect ('home')
