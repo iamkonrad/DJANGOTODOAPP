0d47dce6f098880d3a56211019a2f5a256ec98a8
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-11 18:28:45 +0200

    Completed tasks functionality
    -creating completed_tasks variable inside main_app home function, afterwards passing it into the context
    
    -adding a for loop in home.html that will go over completed_tasks and afterwards displaying the current task within the loop,
    completed_tasks are both added into a database and shown on the homepage

--- main_app/main_app/views.py
+++ main_app/main_app/views.py
@@ -4,7 +4,10 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False). order_by('-updated_at')                                                                     #grabbing the tasks that are not complete
+
+    completed_tasks = Task.objects.filter(is_completed=True)
     context = {                                                                                                         #filter will grab all, get only one
         'tasks':tasks,
+        'completed_tasks':completed_tasks,
     }
     return render(request,'home.html', context)
